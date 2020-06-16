9cc7046a97f01f3d74980cf1394137e95793ef4d
Ming Li <ming.li@sjcminglimx0-nb.local>
2020-06-16 11:06:13 -0700

    06/16 First commit: parsed the pubtator file

---
+++ src/Utils.py
@@ -0,0 +1,23 @@
+__author__ = 'ml4076@columbia.edu'
+
+##########################
+# Useful Functions #
+##########################
+
+import datetime
+import time
+from functools import wraps
+from typing import Any, Callable
+
+
+def timeit(func: Callable[..., Any]) -> Callable[..., Any]:
+    """Times a function, usually used as decorator"""
+    @wraps(func)
+    def timed_func(*args: Any, **kwargs: Any) -> Any:
+        """Returns the timed function"""
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        elapsed_time = datetime.timedelta(seconds=(time.time() - start_time))
+        print("time spent on %s: %s", func.__name__, elapsed_time)
+        return result
+    return timed_func
